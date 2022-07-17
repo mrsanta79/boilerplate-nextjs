44c2a506827743cc90485031b865039b7fffb4e0
Santanu Biswas <dev.sbiswas7991@gmail.com>
2022-07-18 02:02:47 +0530

    feat: loader custom color

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 
-export default function Loader({ size }) {
+export default function Loader({ size, color }) {
     return (
-        <span className={`loader animation animate-spin ${size}`}></span>
+        <span className={`loader animation animate-spin ${size}`} style={{ borderLeftColor: color, borderTopColor: color }}></span>
     )
 }
 
 // Props validations
 Loader.propTypes = {
     size: PropTypes.string,
+    color: PropTypes.string,
 }
 Loader.defaultProps = {
     size: 'w-6 h-6',
+    color: '',
 }
