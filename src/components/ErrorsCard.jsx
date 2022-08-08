a6cde1722b601312912262d501730558567b1d82
Santanu Biswas <dev.sbiswas7991@gmail.com>
2022-08-08 12:05:43 +0530

    fix: errors card UI fix

--- src/components/ErrorsCard.jsx
+++ src/components/ErrorsCard.jsx
@@ -4,11 +4,13 @@ export default function ErrorsCard({ errors, onClear }) {
     return (
         errors
         ? <div className="flex flex-row justify-between items-start relative bg-red-200 border border-red-500 text-red-600 rounded-global p-2">
-            {
-                typeof errors !== 'string' && errors.length
-                ? errors.map((err, index) => <h1 key={index} className={`block ${index !== errors.length - 1 ? 'mb-2' : ''}`}>{err}</h1>)
-                : <h1 className="block">{errors}</h1>
-            }
+            <div className="flex flex-col">
+                {
+                    typeof errors !== 'string' && errors.length
+                    ? errors.map((err, index) => <h1 key={index} className={`block ${index !== errors.length - 1 ? 'mb-2' : ''}`}>{err}</h1>)
+                    : <h1 className="block">{errors}</h1>
+                }
+            </div>
             <span className="relative text-red-600 cursor-pointer" onClick={onClear}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
