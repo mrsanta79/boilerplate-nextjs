049de611fe6d00c18b49ffff2ff74b0e26a1eba2
Santanu Biswas <dev.sbiswas7991@gmail.com>
2022-08-14 02:37:22 +0530

    fix: minor warning fix

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,9 +8,9 @@ export default function Document() {
 
 				{/* Prefetch for faster font loading */}
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-				<link rel="dns-prefetch" href="https://fonts.gstatic.com" crossorigin />
+				<link rel="dns-prefetch" href="https://fonts.gstatic.com" crossOrigin="true" />
 
 				{/* Metadata */}
 				<meta name="mobile-web-app-capable" content="yes" />
