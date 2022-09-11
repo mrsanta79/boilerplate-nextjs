f7800dccd5c7268d6430d4a37d16b9ae68a3515c
Santanu Biswas <dev.sbiswas7991@gmail.com>
2022-09-11 16:08:31 +0530

    fix: comment out manifest

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -27,7 +27,7 @@ export default function Document() {
 				<meta name="twitter:creator" content="@sbiswas1805" />
 
 				{/* PWA Config */}
-				<link rel="manifest" href="/manifest.json" />
+				{/* <link rel="manifest" href="/manifest.json" />
 				<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120-precomposed.png" />
 				<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152-precomposed.png" />
 				<link rel="apple-touch-startup-image" href="/splash-screens/splash-screen-640-x-1136.jpg" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
@@ -36,7 +36,7 @@ export default function Document() {
 				<link rel="apple-touch-startup-image" href="/splash-screens/splash-screen-1242-x-2208.jpg" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
 				<link rel="apple-touch-startup-image" href="/splash-screens/splash-screen-1536-x-2048.jpg" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
 				<link rel="apple-touch-startup-image" href="/splash-screens/splash-screen-1668-x-2224.jpg" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
-				<link rel="apple-touch-startup-image" href="/splash-screens/splash-screen-2048-x-2732.jpg" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
+				<link rel="apple-touch-startup-image" href="/splash-screens/splash-screen-2048-x-2732.jpg" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" /> */}
 			</Head>
 
 			<body className="bg-blue-50 dark:bg-darkest font-sans antialiased">
