e8bdcb9b076ec7cf6cbb667c0de080ef596be750
Santanu Biswas <dev.sbiswas7991@gmail.com>
2023-02-27 23:16:50 +0530

    fix: tailwind modules directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const colors = require('tailwindcss/colors');
 module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
