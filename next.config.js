fcd65121ba126ec1eeb4a396626f74380cf2c097
Santanu Biswas <dev.sbiswas7991@gmail.com>
2023-08-25 12:59:24 +0530

    feat: .env variables rename

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   output: 'standalone',
   compress: true,
   pageExtensions: ['js', 'ts', 'jsx', 'tsx'],
+  env: {
+    APP_NAME: process.env.APP_NAME,
+    APP_URL: process.env.APP_URL,
+    API_URL: process.env.API_URL,
+  }
 }
